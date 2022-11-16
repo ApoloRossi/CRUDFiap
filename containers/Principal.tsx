796b0d84d6d050955d066a01b777753b3d7bea53
Apolo R <apolorossi@gmail.com>
2022-11-16 00:05:19 -0300

    feat: Ajuste de place holder na data.

--- containers/Principal.tsx
+++ containers/Principal.tsx
@@ -115,8 +115,11 @@ export const Principal : NextPage<PrincipalProps> = ({setAccessToken}) =>{
                         {error && <p className='error'>{error}</p>}
                         <input type="text" placeholder='Nome da tarefa'
                             value={name} onChange={e => setName(e.target.value)}/>
-                        <input type="date" placeholder='Data de previsão da tarefa'
-                            value={previsionDate} onChange={e => setPrevisionDate(e.target.value)}/>
+                        <input type="text" placeholder='Data de previsão da tarefa' 
+                            value={previsionDate} 
+                            onFocus={e => e.target.type = "date"}
+                            onChange={e => setPrevisionDate(e.target.value)}
+                            onBlur={(e) => { if(e.target.value == "") (e.target.type = "text")}}/>
                 </Modal.Body>
                 <Modal.Footer>
                     <div className='button col-12'>
